e4d4b7a7f2c00b1a8d0ce76dc3c10a553c5ec336
kanghuawu <kang-hua.wu@sjsu.edu>
2018-01-25 17:09:03 -0800

    1. remove .idea files 2. refact DishMain 3. create FactoryMain

---
+++ java9/src/main/java/java8_in_action/ch08/FactoryMain.java
@@ -0,0 +1,64 @@
+package java8_in_action.ch08;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+public class FactoryMain {
+    static interface Product {}
+    static class Loan implements Product {
+        @Override
+        public String toString() {
+            return "This is a loan";
+        }
+    }
+    static class Stock implements Product {
+        @Override
+        public String toString() {
+            return "This is a stock";
+        }
+    }
+    static class Bond implements Product {
+        @Override
+        public String toString() {
+            return "This is a bond";
+        }
+    }
+
+    static class ProductFactory {
+        static Product createProduct(String name) {
+            switch (name) {
+                case "loan": return new Loan();
+                case "stock": return new Stock();
+                case "bond": return new Bond();
+                default: throw new IllegalArgumentException();
+            }
+        }
+
+        static Supplier<Product> createProductLambda(String name) {
+            Supplier<Product> re = map.get(name);
+            if (re != null) return re;
+            throw new IllegalArgumentException();
+        }
+    }
+
+    final static Map<String, Supplier<Product>> map = new HashMap<>();
+    static {
+        map.put("loan", Loan::new);
+        map.put("stock", Stock::new);
+        map.put("bond", Bond::new);
+    }
+
+    public static void main(String[] args) {
+        Product loan1 = ProductFactory.createProduct("loan");
+        System.out.println(loan1);
+
+        Supplier<Product> loanSupplier = Loan::new;
+        Product loan2 = loanSupplier.get();
+        System.out.println(loan2);
+
+        Supplier<Product> loan3 = ProductFactory.createProductLambda("loan");
+        System.out.println(loan3.get());
+        System.out.println(loan3.hashCode());
+    }
+}
--- java9/src/main/java/java8_in_action/ch08/Main.java
+++ java9/src/main/java/java8_in_action/ch08/Main.java
@@ -1,26 +1,24 @@
 package java8_in_action.ch08;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
-import java8_in_action.ch08.Dish.CaloricLevel;
+import java8_in_action.ch08.DishMain.CaloricLevel;
 
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.summingInt;
-import static java8_in_action.ch08.Dish.menu;
+import static java8_in_action.ch08.DishMain.menu;
 
 public class Main {
     public static void main(String[] args) {
-        Map<CaloricLevel, List<Dish>> dishesByCaloricLevel = menu.stream()
-                .collect(groupingBy(Dish::getCaloricLevel));
+        Map<CaloricLevel, List<DishMain>> dishesByCaloricLevel = menu.stream()
+                .collect(groupingBy(DishMain::getCaloricLevel));
         System.out.println(dishesByCaloricLevel);
         int totalCalories = menu.stream()
-                .map(Dish::getCalories)
+                .map(DishMain::getCalories)
                 .reduce(0, (d1, d2) -> d1 + d2);
         System.out.println(totalCalories);
         totalCalories = menu.stream()
-                .collect(summingInt(Dish::getCalories));
+                .collect(summingInt(DishMain::getCalories));
         System.out.println(totalCalories);
 
 
