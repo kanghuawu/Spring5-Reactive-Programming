67dfc4e405765f7fdb9a8da9f6c194ce6ba36a50
kanghuawu <kang-hua.wu@sjsu.edu>
2018-01-30 23:40:36 -0800

    addtional practice for optional

---
+++ java9/src/main/java/java8_in_action/ch10/mkyoung/com/OptionalFilterMain.java
@@ -0,0 +1,14 @@
+package java8_in_action.ch10.mkyoung.com;
+
+import java.util.Optional;
+
+public class OptionalFilterMain {
+    public static void main(String[] args) {
+        Optional<String> gender = Optional.of("MALE");
+        Optional<String> emptyGender = Optional.empty();
+
+        System.out.println(gender.filter(g -> g.equals("male")));
+        System.out.println(gender.filter(g -> g.equalsIgnoreCase("male")));
+        System.out.println(emptyGender.filter(g -> g.equalsIgnoreCase("male")));
+    }
+}
