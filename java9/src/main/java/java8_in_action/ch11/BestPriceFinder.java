3a0b907979df167a8253219e589e6e221c3fc4bc
kanghuawu <kang-hua.wu@sjsu.edu>
2018-02-01 22:40:58 -0800

    ch11

---
+++ java9/src/main/java/java8_in_action/ch11/BestPriceFinder.java
@@ -0,0 +1,84 @@
+package java8_in_action.ch11;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+import static java.util.stream.Collectors.toList;
+
+import java8_in_action.ch11.ExchangeService.Money;
+
+public class BestPriceFinder {
+
+    private final List<Shop> shops = Arrays.asList(new Shop("BestPrice"),
+                                                    new Shop("LetsSaveBig"),
+                                                    new Shop("MyFavoriteShop"),
+                                                    new Shop("BuyItAll"),
+                                                    new Shop("BuyEasy"));
+
+    private final Executor executor = Executors.newFixedThreadPool(Math.min(shops.size(), 100), new ThreadFactory() {
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(r);
+            t.setDaemon(true);
+            return t;
+        }
+    });
+    public List<String> findPrices(String product) {
+        return shops.stream()
+                .map(shop -> String.format("%s price is %.2f", shop.getName(), shop.getPrice(product)))
+                .collect(toList());
+    }
+
+    public List<String> findPricesParallel(String product) {
+        return shops.parallelStream()
+                .map(shop -> String.format("%s price is %.2f", shop.getName(), shop.getPrice(product)))
+                .collect(toList());
+    }
+
+    public List<String> findPriceFutures(String product) {
+        List<CompletableFuture<String>> priceFutures = shops.stream()
+                .map(shop -> CompletableFuture.supplyAsync(
+                        () -> String.format("%s price is %.2f", shop.getName(), shop.getPrice(product)), executor))
+                .collect(toList());
+        return priceFutures.stream().map(CompletableFuture::join).collect(toList());
+    }
+
+    public List<String> findPriceWithQuotesDiscount(String product) {
+        return shops.stream()
+                .map(shop -> shop.getPriceWithDiscount(product))
+                .map(Quote::parse)
+                .map(Discount::applyDiscount)
+                .collect(toList());
+    }
+
+    public List<String> findPriceWithQuoteDiscountFuture(String product) {
+         List<CompletableFuture<String>> prices = shops.stream()
+                .map(shop -> CompletableFuture.supplyAsync(() -> shop.getPriceWithDiscount(product), executor))
+                .map(future -> future.thenApply(Quote::parse))
+                .map(future -> future.thenCompose(quote ->
+                        CompletableFuture.supplyAsync(
+                        () -> Discount.applyDiscount(quote), executor)))
+                .collect(toList());
+        return prices.stream()
+                .map(CompletableFuture::join)
+                .collect(toList());
+    }
+
+    public List<String> findPriceInUSD(String product) {
+        List<CompletableFuture<String>> priceFutures = shops.stream()
+                .map(shop -> CompletableFuture
+                    .supplyAsync(() -> shop.getPrice(product), executor)
+                    .thenCombine(
+                            CompletableFuture.supplyAsync(() -> ExchangeService.getRate(Money.EUR, Money.USD), executor),
+                            (price, rate) -> price * rate)
+                    .thenApply(price -> String.format("%s price is %.2f", shop.getName(), price)))
+                .collect(toList());
+        return priceFutures.stream()
+                .map(CompletableFuture::join)
+                .collect(toList());
+    }
+}
